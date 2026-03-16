c66ee01c1608b97ae083d7e9db03b8dd512e8c7d
Matthew Yeh <matthewyeh2006@gmail.com>
2026-03-16 03:33:05 -0700

    Standardized schema SQL defaults, fixed formatting errors, adjusted char limits

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,4 +1,4 @@
---v2
+-- v2
 
 CREATE TABLE IF NOT EXISTS Farmers (
     farmer_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -16,10 +16,10 @@ CREATE TABLE IF NOT EXISTS Rover_Sessions (
     farmer_id INT NOT NULL,
     session_date DATE NOT NULL,
     started_at TIMESTAMP NULL,
-    stopped_at TIMESTAMP NOT NULL,
-    status ENUM('Running', 'Stopped', 'Processing', 'Completed') NOT NULL DEFAULT 'IDLE',
+    stopped_at TIMESTAMP NULL,
+    status ENUM('Running', 'Stopped', 'Processing', 'Completed') NOT NULL DEFAULT 'Stopped',
     last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    active_command ENUM('idle', 'stop', 'start') NOT NULL DEFAULT 'IDLE',
+    active_command ENUM('idle', 'stop', 'start') NOT NULL DEFAULT 'idle',
     
     -- This creates the "link" to the Farmers table
     FOREIGN KEY (farmer_id) REFERENCES Farmers(farmer_id)
@@ -30,10 +30,10 @@ CREATE TABLE IF NOT EXISTS Scans (
     session_id INT NOT NULL,
     farmer_id INT NOT NULL,
     disease_status ENUM('DISEASED', 'HEALTHY', 'NO PLANT') NOT NULL,
-    image_url VARCHAR(225) NOT NULL,
-    image_key VARCHAR(225) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    image_key VARCHAR(255) NOT NULL,
     severity ENUM('YELLOW', 'RED', 'ORANGE') NOT NULL,
-    gemini_status VARCHAR(10),
+    gemini_status VARCHAR(20),
     scanned_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     gps_lat DOUBLE NOT NULL,
     gps_lng DOUBLE NOT NULL,
