af776efd51c82cd8b09467230821d08a3fb8c67e
Mukta Mahabaleshwar <muktamahabaleshwar@MacBook-Pro-9.local>
2026-04-05 18:13:38 -0700

    update rover endpoints and service

--- backend/app/services/rover_service.py
+++ backend/app/services/rover_service.py
@@ -6,30 +6,30 @@ from app.services.mqtt_service import publish_command
 def start_rover (db: Session, session_id:int) -> dict:
     session= db.query (RoverSession).filter(RoverSession.session_id== session_id).first()
     if not session :
-        raise ValueError == (f"Session {session_id} not found")
-    if session.start:
+        raise ValueError (f"Session {session_id} not found")
+    if session.start =="Running":
         raise ValueError ("Rover is already running")
     
-    session.status= "running"
+    session.status= "Running"
     session.started_at= datetime.now(timezone.utc)
     session.active_command= "start"
     db.commit()
 
-    publish_command(session_id=session_id, command="start")
+    publish_command(command = "start", rover_id = "session_id")
 
     return {"message": f"Rover started for session {session_id}", "status": "Running"}
 def stop_rover (db: Session, session_id:int ) -> dict:
     session= db.query (RoverSession).filter(RoverSession.session_id== session_id).first()
     if not session :
-        raise ValueError == (f"Session {session_id} not found")
-    if session.start:
+        raise ValueError(f"Session {session_id} not found")
+    if session.start == "Stopped":
         raise ValueError ("Rover is already stopped")
 
     session.status= "Stopped"
-    session.started_at= datetime.now(timezone.utc)
+    session.stopped_at= datetime.now(timezone.utc)
     session.active_command= "stop"
     db.commit()
 
-    publish_command(session_id=session_id, command="stop")
+    publish_command(command="stop",rover_id= "session_id")
 
     return {"message": f"Rover stopped for session {session_id}", "status": "Stopped"}
